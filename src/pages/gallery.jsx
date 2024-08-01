ff8e2fb7ee6848f41e054b41529a4af999653e62
kartik <kartik@kali.kartik>
2024-08-01 20:35:50 +0100

    Added code

---
+++ src/pages/gallery.jsx
@@ -0,0 +1,13 @@
+// src/pages/gallery.jsx
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <div>
+      <h1>Gallery Page</h1>
+      <p>This is a placeholder for the gallery page. Content will be added soon.</p>
+    </div>
+  );
+};
+
+export default Gallery;
